595ebb404141caf6b90b695e0e074e1c16f7eb88
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-04-18 14:10:36 +0300

    feat: add watch gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -66,3 +66,9 @@ gulp.task("clean", function (done) {
 	}
 	done();
 });
+
+gulp.task("watch", function () {
+	gulp.watch("./src/scss//*.scss", gulp.parallel("sass"));
+	gulp.watch("./src//*.html", gulp.parallel("html"));
+	gulp.watch("./src/img//*", gulp.parallel("images"));
+});
