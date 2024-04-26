3d8cb5bf86aaae9de215c424abd771ae7998620e
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-04-26 17:56:10 +0300

    feat: test

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
-console.log("hello");
+const burger = document.querySelector(".burger");
+const btn = document.querySelectorAll(".btn");
+
+burger.addEventListener("click", () => alert("Hellooo"));
+btn.forEach((el) => el.addEventListener("click", () => alert("button click")));
